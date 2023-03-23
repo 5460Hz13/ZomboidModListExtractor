bf5e06fe9ad619144052796f20474b76ca944ca5
5460Hz13 <60887319+5460Hz13@users.noreply.github.com>
2023-03-23 14:23:56 +0000

    Add files via upload

---
+++ gatherIdsToFile.ps1
@@ -0,0 +1,5 @@
+﻿$folderList = Get-ChildItem -Directory | Select-Object -ExpandProperty Name
+
+$folderList -join ';' | Out-File -FilePath "extractedIds.txt"
+
+Write-Host "Folder names copied to folderList.txt"
---
+++ gatherNamesToFile.ps1
@@ -0,0 +1,10 @@
+$modFiles = Get-ChildItem -Path .\*\mods\*\mod.info -Recurse
+$idValues = @()
+foreach ($modFile in $modFiles) {
+    $modContent = Get-Content $modFile.FullName
+    $idValue = $modContent | Select-String -Pattern 'id=(.*)' | % { $_.Matches.Groups[1].Value }
+    if ($idValue) {
+        $idValues += $idValue
+    }
+}
+Set-Content -Path .\extractedNames.txt -Value ($idValues -join ';')
